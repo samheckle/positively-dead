92d03778f113abf67253f5652d19de2193306a16
Nikolas Whiteside <nrw6218@rit.edu>
2018-09-04 12:10:35 -0500

    Added script files and created a dialogue test scene with a canvas object and some sample text.

---
+++ Positively Dead/Assets/Scripts/DialogueParser.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DialogueParser : MonoBehaviour {
+
+	public Text inspectText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
