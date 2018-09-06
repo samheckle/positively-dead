885a2e0429fa7fccff2d3c06c259ba7016abe477
Nikolas Whiteside (RIT Student) <nrw6218@ad.rit.edu>
2018-09-06 10:18:37 -0400

    Start of dialogue system. Creates Dialogue and DialogueManager classes as well as puts some basic text on screen.

---
+++ Positively Dead/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue {
+
+    // Dialogue information
+    private string text = "";
+    private string speakerName = "";
+
+    // Animation information
+    private float textSpeed;
+
+    public string Text {
+        get { return text; }
+        set { text = value; }
+    }
+
+    public string SpeakerName
+    {
+        get { return speakerName; }
+        set { speakerName = value; }
+    }
+
+    public float TextSpeed
+    {
+        get { return textSpeed; }
+        set { textSpeed = value; }
+    }
+
+    // Use this for initialization
+    public Dialogue(string speakerName, string text) {
+        this.speakerName = speakerName;
+        this.text = text;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Positively Dead/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    private Dialogue currentDialogue;
+    private List<Dialogue> dialogues;
+
+    [SerializeField]
+    private string sceneName;
+
+    [SerializeField]
+    private Text dialogueBox;
+
+    [SerializeField]
+    private Text speakerName;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("scene has loaded");
+        /* 
+         * Open file based on scene name
+         * Parse through for character name, text and speed
+         * Save each block as it's own dialogue object in the dictionary
+         * FOR SAKE OF DEMO - kickstart dialogue runthrough
+         */
+        dialogues = new List<Dialogue>();
+
+        dialogues.Add(new Dialogue("Nik", "Yo, what's up?"));
+        dialogues.Add(new Dialogue("Sam", "*Mutters something sarcastic under breath*"));
+        dialogues.Add(new Dialogue("Nik", "*Leaves and goes to Target where he lives*"));
+
+        Debug.Log(dialogues[0]);
+        dialogueBox.text = dialogues[0].Text;
+        speakerName.text = dialogues[0].SpeakerName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
