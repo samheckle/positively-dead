7bfab125bac608bd96358a8520510b8ba120f63f
Nikolas Whiteside <nrw6218@rit.edu>
2018-09-06 23:18:43 -0400

    Added typewriter animation for dialogue. Created dialogue node and dialogue manager. Basic tree traversal built in with response buttons.

--- Positively Dead/Assets/Scripts/Dialogue.cs
+++ Positively Dead/Assets/Scripts/Dialogue.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Author: Nikolas Whiteside
+/// Date: September 6, 2018
+/// Description: A data container for dialogue.
+/// </summary>
 public class Dialogue {
 
     // Dialogue information
@@ -11,6 +16,10 @@ public class Dialogue {
 
     // Animation information
     private float textSpeed;
+
+    // Dialogue tree options
+    private List<Dialogue> dialogueOptions;
+    private List<string> responseOptions;
 
     public string Text {
         get { return text; }
@@ -29,14 +38,39 @@ public class Dialogue {
         set { textSpeed = value; }
     }
 
+    public List<string> ResponseOptions
+    {
+        get { return responseOptions; }
+        set { responseOptions = value; }
+    }
+
+    public int PossibleResponses
+    {
+        get { return responseOptions.Count; }
+    }
+
     // Use this for initialization
-    public Dialogue(string speakerName, string text) {
+    public Dialogue (string speakerName, string text, float speed, List<Dialogue> dialogueOptions = null, List<string> responseOptions = null) {
         this.speakerName = speakerName;
         this.text = text;
+        this.textSpeed = speed;
+        this.dialogueOptions = dialogueOptions;
+        this.responseOptions = responseOptions;
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    /// <summary>
+    /// Returns a response to the dialogue given an index
+    /// </summary>
+    public Dialogue NextDialogue (int index = 0) {
+        if (dialogueOptions != null) {
+            return dialogueOptions[index];
+        } else {
+            return null;
+        }
+    }
 }
---
+++ Positively Dead/Assets/Scripts/Typewriter.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+/// <summary>
+/// Author: Nikolas Whiteside
+/// Date: April 25, 2017
+/// Description: Animates text on the hud like a typewriter
+/// </summary>
+public class Typewriter : MonoBehaviour {
+
+	//The text to be animated
+	private Text animText;
+
+	//The final string to display
+	public string finalText;
+
+	//Test to see if the string has changed
+	private string testText;
+
+	//The typing delay
+	public float typeDelay;
+
+    public bool animComplete;
+
+	// Use this for initialization
+	void Awake () {
+		animText = gameObject.GetComponent<Text> ();
+		StartCoroutine (Type ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Check whether the string has changed
+		if (finalText != testText) {
+			//Reset the text and begin the animation again
+			StopAllCoroutines();
+			animText.text = "";
+			StartCoroutine(Type ());
+		}
+
+		//Set the test equal to the desired string
+		testText = finalText;
+	}
+
+	/// <summary>
+	/// Types out the text
+	/// </summary>
+	IEnumerator Type(){
+        animComplete = false;
+		for (int i = 0; i < finalText.Length + 1; i++) {
+			animText.text = finalText.Substring(0, i);
+            Debug.Log("wut");
+			yield return new WaitForSeconds(typeDelay);
+		}
+        animComplete = true;
+	}
+}
