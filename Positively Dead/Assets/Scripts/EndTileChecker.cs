7d18f566e04fd75d8ea88e6504ef3b8084ba03f2
Israel Anthony <ixa4371@rit.edu>
2018-11-27 16:37:38 -0500

    Created levels based on the mock ups

---
+++ Positively Dead/Assets/Scripts/EndTileChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTileChecker : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Vector3.Distance(this.gameObject.transform.position,player.transform.position) <= 0.2f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+	}
+}
